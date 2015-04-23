76c8602cdabb57c262198c354b8390edbcfa854f
KyrstanZachary <kyrstan.zachary@gmail.com>
2015-04-22 22:20:24 -0700

    First commit

---
+++ teacherResources.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Krissy Zachary</title>
+		<meta charset = "UTF-8">
+		<link rel="stylesheet" type="text/css" href="Styles.css">
+		<link href='http://fonts.googleapis.com/css?family=The+Girl+Next+Door|Over+the+Rainbow|Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div id="bodyBackground">
+		<div id="wrapper">
+			<div id= "header">
+				<div id="headerBox">
+					<p>Ms. Zachary's Classroom
+					</p>	
+				</div>
+			</div>	
+				
+				<div id="topNavContainer">	
+				<a href = "index.html"><div class="topNavButton">Home</div></a><br>
+				<a href = "schedule.html"><div class="topNavButton">Unit Schedules</div></a><br>
+				<a href = "assignments.html"><div class="topNavButton">Assignments</div></a><br>
+				<a href = "projectExamples.html"><div class="topNavButton">Project Examples</div></a><br>
+				<a href = "resources.html"><div class ="topNavButton">Student Resources</div></a><br>
+				<a href = "contact.html"><div class="topNavButton">Contact</div></a><br>
+				<a href = "teacherResources.html"><div class="topNavButton">Teacher Resources</div></a><br>
+				</div>
+			<div id="teacherResourcesContainerHeader" class="containerHeaders">
+				<p>Teacher Resources
+				</p>	
+			</div>	
+			<div id="teacherResourcesUnitContainer" div class="unitContainer">
+				<a href = "lessonPlans.html"><div class="unitButton">Lesson Plans</div></a><br>
+				<a href = "domains.html"><div class="unitButton">Learning Styles, Domains & Theories</div></a><br>
+				<a href = "educationalVideos.html"><div class="unitButton">Educational Videos</div></a><br>
+				<a href = "worksheets.html"><div class="unitButton">Worksheets</div></a><br>
+				<a href = "websites.html"><div class="unitButton">Websites</div></a>
+				<?php
+					echo "<p>This is my blog!</p>";
+				?>
+			</div>
+		</div>
+	</body>
+</html>	
+			<div id="resourceDisclaimer">
+				<p><u><i>A note to the reader:</i></u><br>
+					The resources you will find here are a compilation of handouts, lesson plans, and other items that I have found to be helpful throughout my journey to becoming a teacher. 
+			</div>
+		</div>	
+	</body>
+</html>		
